1cf121dfe5e59c163176bd0a1efc868451cfeee4
williamEkim <dongyeon0308@gmail.com>
2026-02-11 18:03:08 -0800

    quickfix

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from src.logic.dataclean import dataclean
 
 def main():
-    method = input("Choose the Method: (AABB / OBB / PCA)\n   -->  ")
-    # dataclean("src/data/8.ply", True, method)
-    # dataclean("src/data/9.ply", True, method)
-    dataclean("src/data/sample2.ply", True, method, verbose=True)
+    method = input("Choose the Method: (AABB / OBB)\n   -->  ")
+    # dataclean("src/data/8.ply", True, method, verbose=True)
+    dataclean("src/data/9.ply", True, method, verbose=True)
+    # dataclean("src/data/sample2.ply", True, method, verbose=True)
 
 if __name__ == "__main__":
     main()
